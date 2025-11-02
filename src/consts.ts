d82c817598641061e8e118d582b31a69080accbb
Philipp Isaak <phi.isaak@gmail.com>
2025-11-02 16:20:12 +0100

    setup daisy ui and taiwlind

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'KLK - Kultur Like Küssnacht';
+export const SITE_DESCRIPTION = 'Erlebe Kultur in Küssnacht: Veranstaltungen, Konzerte, Ausstellungen und mehr – präsentiert von Kultur Like Küssnacht (KLK). Entdecke das aktuelle Programm!';
